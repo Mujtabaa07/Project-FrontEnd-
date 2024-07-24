54000b0ddebb6219e33ff5d44323454d56c7cfb4
Mohamed Mujtaba <mohamedmujtaba07@gmail.com>
2024-07-24 22:48:00 +0530

    added loader

---
+++ scrpits.js
@@ -0,0 +1,51 @@
+// var h5timer = document.querySelector("#line1-part1 h5");
+// var grow = 0;
+// setInterval(function () {
+//   if (grow < 100) {
+//     h5timer.innerHTML = grow++;
+//   } else {
+//     h5timer.innerHTML = grow;
+//   }
+// }, 35);
+
+var tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.2,
+  opacity: 0,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 35);
+  },
+});
+tl.to(".line h2", {
+  animationName: "animation",
+  opacity: 1,
+});
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 3.8,
+});
+tl.from("#page1", {
+  delay: 0.2,
+  y: 1600,
+  opacity: 0,
+  duration: 0.5,
+  ease: Power4,
+});
+tl.to("#loader", {
+  display: "none",
+});
