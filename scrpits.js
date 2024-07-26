a685dd51e22bf9c9bd725fd93388bbfab1a44570
Mohamed Mujtaba <mohamedmujtaba07@gmail.com>
2024-07-26 11:59:16 +0530

    page4 made

--- scrpits.js
+++ scrpits.js
@@ -1,9 +1,8 @@
-function LoadingAnimation() {
+function loadingAnimation() {
   var tl = gsap.timeline();
   tl.from(".line h1", {
     y: 150,
-    stagger: 0.2,
-    opacity: 0,
+    stagger: 0.25,
     duration: 0.6,
     delay: 0.5,
   });
@@ -18,37 +17,35 @@ function LoadingAnimation() {
         } else {
           h5timer.innerHTML = grow;
         }
-      }, 35);
+      }, 33);
     },
   });
   tl.to(".line h2", {
-    animationName: "animation",
+    animationName: "anime",
     opacity: 1,
   });
   tl.to("#loader", {
     opacity: 0,
     duration: 0.2,
-    delay: 3.8,
+    delay: 0,
   });
   tl.from("#page1", {
     delay: 0.2,
     y: 1600,
-    opacity: 0,
-    duration: 0.5,
+    duration: 0.6,
     ease: Power4,
   });
   tl.to("#loader", {
     display: "none",
   });
-  tl.from("#nav", {
-    opacity: 0,
-  });
-  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
-    y: 120,
-    stagger: 0.2,
-  });
+  tl.from("#nav",{
+    opacity:0
+  })
+  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:140,
+    stagger:0.2
+  })
 }
-
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
     gsap.to("#crsr", {
@@ -56,7 +53,8 @@ function cursorAnimation() {
       top: dets.y,
     });
   });
-  Shery.makeMagnet("#nav-part2 h4", {});
+
+  Shery.makeMagnet("#nav-part2 h4");
 }
-LoadingAnimation();
-cursorAnimation();
+loadingAnimation();
+cursorAnimation()
